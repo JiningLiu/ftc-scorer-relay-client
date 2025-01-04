6a678f3d256d654feab70c0025b9f561757790f9
Jining Liu <jiningdev@gmail.com>
2025-01-04 12:21:30 -0600

    ui & updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,6 @@
 	const prodServerWsUrl = 'wss://relay.ftcscoring.app';
 	const devServerWsUrl = 'ws://localhost:20240';
 
-	let storage = false;
-
 	let user = '';
 	let password = '';
 	let eventCode = '';
@@ -26,14 +24,13 @@
 		'SHOW_RESULTS'
 	];
 
-	onMount(() => {});
+	onMount(() => {
+		// user = localStorage.getItem('user') || '';
+		// password = localStorage.getItem('password') || '';
+	});
 
-	function connect() {
-		ftcliveWs?.close();
+	function connectServer() {
 		serverWs?.close();
-
-		// relay server
-
 		serverWs = new WebSocket(development ? devServerWsUrl : prodServerWsUrl, [user, password]);
 
 		serverWs.onopen = () => {
@@ -56,107 +53,118 @@
 			if (serverConnected && newConnStatus) return;
 
 			serverConnected = newConnStatus;
-
-			if (serverConnected) {
-				ftcliveWs = new WebSocket(
-					`ws://${ftcliveWsHost}/stream/display/command/?code=${eventCode}`
-				);
-
-				ftcliveWs.onopen = () => {
-					ftcliveConnected = true;
-				};
-
-				ftcliveWs.onclose = () => {
-					ftcliveConnected = false;
-				};
-
-				ftcliveWs.onmessage = (message) => {
-					console.log(message.data);
-					try {
-						serverWs.send(
-							JSON.stringify({
-								eventCode: eventCode.toLowerCase(),
-								clientType: 'relay',
-								connected: true,
-								data: JSON.parse(message.data)
-							})
-						);
-					} catch (e) {
-						console.log(e);
-					}
-				};
-			}
 		};
 	}
 
-	$: () => {
-		if (storage) {
-			localStorage.setItem('user', user);
-			localStorage.setItem('password', password);
-		} else {
-			localStorage.removeItem('user');
-			localStorage.removeItem('password');
-		}
-	};
+	function connectFtclive() {
+		ftcliveWs?.close();
+		ftcliveWs = new WebSocket(`ws://${ftcliveWsHost}/stream/display/command/?code=${eventCode}`);
+
+		ftcliveWs.onopen = () => {
+			ftcliveConnected = true;
+		};
+
+		ftcliveWs.onclose = () => {
+			ftcliveConnected = false;
+		};
+
+		ftcliveWs.onmessage = (message) => {
+			if (message.data != 'pong') {
+				console.log(message.data);
+				try {
+					serverWs?.send(
+						JSON.stringify({
+							eventCode: eventCode.toLowerCase(),
+							clientType: 'relay',
+							connected: true,
+							data: JSON.parse(message.data)
+						})
+					);
+				} catch (e) {
+					console.log(e);
+				}
+			}
+		};
+	}
 </script>
 
 <main>
 	<div>
-		<input bind:value={user} placeholder="FTC API Username" type="text" name="User" id="user" /><br
-		/>
+		<div class="field-section">
+			<h3>Credentials</h3>
 
-		<input
-			bind:value={password}
-			placeholder="FTC API Password"
-			type="password"
-			name="Password"
-			id="password"
-		/><br /><br />
+			<input bind:value={user} placeholder="FTC API Username" type="text" name="User" id="user" />
 
-		<input
-			bind:value={eventCode}
-			placeholder="Event Code"
-			type="text"
-			name="Event Code"
-			id="event-code"
-		/><br />
+			<input
+				bind:value={password}
+				placeholder="FTC API Password"
+				type="password"
+				name="Password"
+				id="password"
+			/>
+		</div>
 
-		<input
-			bind:value={ftcliveWsHost}
-			placeholder="FTCLive Host"
-			type="text"
-			name="Host"
-			id="host"
-		/><br /><br />
+		<div class="field-section">
+			<h3>Event</h3>
 
-		<input bind:checked={development} type="checkbox" name="Dev" id="dev-toggle" />
-		<label for="dev-toggle">Development</label><br />
+			<input
+				bind:value={eventCode}
+				placeholder="Event Code"
+				type="text"
+				name="Event Code"
+				id="event-code"
+			/>
 
-		{#if ftcliveConnected}
-			<p>FTCLive Connected</p>
-		{/if}
+			<input
+				bind:value={ftcliveWsHost}
+				placeholder="FTCLive Host"
+				type="text"
+				name="Host"
+				id="host"
+			/>
+		</div>
 
-		{#if serverConnected}
-			<p>Server Connected</p>
-		{/if}
+		<div class="field-section">
+			<div class="checkbox">
+				<input bind:checked={development} type="checkbox" name="Dev" id="dev-toggle" />
+				<label for="dev-toggle">Development</label>
+			</div>
 
-		<input bind:checked={storage} type="checkbox" name="storage" id="storage-toggle" />
-		<!-- <label for="storage-toggle">Use LocalStorage to store credentials</label><br /><br /> -->
+			<!-- storage delete -->
+		</div>
 
-		<button on:click={connect}>Connect</button><br /><br />
+		<div class="field-section">
+			<h3 class="conn-status">
+				<span class={serverConnected ? 'on' : 'off'}
+					>• {serverConnected ? 'C' : 'Disc'}onnected</span
+				>{development ? 'Development' : 'Production'} Relay Server
+			</h3>
 
-		<p>Open console to see relays.</p>
+			<button on:click={connectServer}>1 - Connect to Relay Server</button>
+		</div>
+
+		<div class="field-section">
+			<h3 class="conn-status">
+				<span class={ftcliveConnected ? 'on' : 'off'}
+					>• {ftcliveConnected ? 'C' : 'Disc'}onnected</span
+				>FTCLive
+			</h3>
+
+			<button on:click={connectFtclive}>2 - Connect to FTCLive</button>
+		</div>
+		<p>Open console to see data.</p>
 	</div>
 </main>
 
-<style lang="scss">
-	@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+<div id="viewport-too-small">
+	<h1>Window is Too Small</h1>
+	<p>Please make your window larger to use this app.</p>
+</div>
 
+<style lang="scss">
 	:global(body) {
-		overflow: hidden;
 		margin: 0;
 		padding: 0;
-		font-family: 'Poppins', system-ui, sans-serif;
 		color: #fff;
 		background-color: #0f0f0f;
 	}
@@ -167,5 +175,123 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
+
+		div {
+			display: flex;
+			flex-direction: column;
+			gap: 1rem;
+			background-color: #222;
+			padding: 1rem;
+			border-radius: 0.5rem;
+
+			div {
+				padding: 0;
+			}
+
+			.field-section {
+				display: flex;
+				flex-direction: column;
+				gap: 0.5rem;
+			}
+
+			.checkbox {
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+				gap: 0.2rem;
+			}
+		}
+	}
+
+	input {
+		padding: 0.5rem 0.8rem;
+		font-size: 1rem;
+		border: none;
+		border-radius: 0.5rem;
+		background-color: #333;
+		color: #fff;
+	}
+
+	h3,
+	p {
+		margin: 0;
+	}
+
+	h3 {
+		font-size: 1.2rem;
+
+		span {
+			font-weight: 600;
+			margin-right: 0.4rem;
+		}
+	}
+
+	h3 .on {
+		color: #4f4;
+	}
+
+	h3 .off {
+		color: #f44;
+	}
+
+	.conn-status {
+		min-width: 390px;
+		padding: 0.5rem 0.8rem;
+		background-color: #333;
+		border-radius: 0.5rem;
+	}
+
+	button {
+		padding: 0.5rem 1rem;
+		font-size: 1rem;
+		font-weight: 500;
+		border: none;
+		border-radius: 0.5rem;
+		background-color: #193;
+		color: #fff;
+		cursor: pointer;
+		transition: all 0.2s;
+	}
+
+	button:hover {
+		background-color: #082;
+	}
+
+	#viewport-too-small {
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 100vw;
+		height: 100vh;
+		display: none;
+		justify-content: center;
+		gap: 0.5rem;
+		align-items: center;
+		flex-direction: column;
+		background-color: #000;
+
+		* {
+			margin: 0;
+		}
+
+		h1 {
+			font-size: 2rem;
+		}
+
+		p {
+			font-size: 1rem;
+		}
+	}
+
+	@media (max-width: 448px) {
+		#viewport-too-small {
+			display: flex;
+		}
+	}
+
+	@media (max-height: 518px) {
+		#viewport-too-small {
+			display: flex;
+		}
 	}
 </style>
